5fb8e0f283970a50c07c212534a6e91919e736cd
sd41847 <sd41847@inetpsa.com>
2024-12-24 08:50:04 +0000

    update main

--- main.py
+++ main.py
@@ -12,16 +12,19 @@ if __name__ == "__main__":
     article_info_excel = Path(r"data/DONNEES DOUANE PYTHON.xlsx")
     article_info = Article_Info(source_excel=article_info_excel)
 
-    #* chang pdf path here
+    #* add pdf path here
+    pdf_path_list = [
+        Path(r"data/Facture 01-11 au 15-11.pdf"),
+        Path(r"data/Facture 16-11 au 30-11.pdf"),
+    ]
 
-    # pdf_path = Path(r"data/Facture 01-11 au 15-11.pdf")
-    pdf_path = Path(r"data/Facture 16-11 au 30-11.pdf")
-    log_file_path = output_folder_path / f"{pdf_path.stem}.log"
-    if log_file_path.exists():
-        # clean existed log file
-        log_file_path.unlink()
+    for pdf_path in pdf_path_list:
+        log_file_path = output_folder_path / "log" / f"{pdf_path.stem}.log"
+        if log_file_path.exists():
+            # one log file per pdf, and clean existed log file
+            log_file_path.unlink()
 
-    logger.add(log_file_path, level="INFO")
+        logger.add(log_file_path, level="INFO")
 
-    x = IviviFactureReader(pdf_path=pdf_path, article_info=article_info, output_folder_path=output_folder_path)
-    x.run()
+        x = IviviFactureReader(pdf_path=pdf_path, article_info=article_info, output_folder_path=output_folder_path)
+        x.run()
