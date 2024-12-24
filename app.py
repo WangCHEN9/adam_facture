008cc88792f54e7237c5321861a5f67b24d84653
sd41847 <sd41847@inetpsa.com>
2024-12-24 10:20:00 +0000

    complete

--- app.py
+++ app.py
@@ -10,6 +10,15 @@ def main():
     st.title("PDF to XML Processor")
     st.write("Upload a PDF file to process and generate XML and logs.")
 
+    # Sidebar for company selection
+    st.sidebar.title("Company Selection")
+    company_name = st.sidebar.selectbox("Select Company", ["IVIVI"])
+
+    func_mapping = {
+        "IVIVI": IviviFactureReader,
+    }
+    process_func = func_mapping[company_name]
+
     # Set up paths
     output_folder_path = Path("output")
     output_folder_path.mkdir(parents=True, exist_ok=True)
@@ -43,7 +52,8 @@ def main():
         # Process the PDF
         st.write("Processing the PDF... It will take few minutes")
         try:
-            ivivi_reader = IviviFactureReader(
+
+            ivivi_reader = process_func(
                 pdf_path=temp_pdf_path, 
                 article_info=article_info, 
                 output_folder_path=output_folder_path
