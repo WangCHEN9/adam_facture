2ccf3b3b2a4c973a776ee61690943e9e439e1d9b
Wang CHEN <solid.chen9@gmail.com>
2025-04-27 13:25:35 +0200

    update

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from ivivi_facture_reader import IviviFactureReader
 from jessy_facture_reader import JessyFactureReader
 from dolvika_facture_reader import DolvikaFactureReader
 from mod_facture_reader import ModFactureReader
+from sarl_zhc_facture_reader import SarlZhcFactureReader
 from loguru import logger
 import pandas as pd
 
@@ -16,7 +17,7 @@ if __name__ == "__main__":
     article_info = Article_Info(source_excel=article_info_excel)
 
     # pdf_path = Path(r"input/DOLVIKA S4FW.pdf")
-    pdf_path = Path(r"input/JESSY & CO L5B7.pdf")
+    pdf_path = Path(r"input/sarl_zhc.pdf")
     log_file_path = output_folder_path / "log" / f"{pdf_path.stem}.log"
     if log_file_path.exists():
         # one log file per pdf, and clean existed log file
@@ -24,7 +25,7 @@ if __name__ == "__main__":
 
     logger.add(log_file_path, level="INFO")
 
-    x = JessyFactureReader(pdf_path=pdf_path, article_info=article_info, output_folder_path=output_folder_path)
+    x = SarlZhcFactureReader(pdf_path=pdf_path, article_info=article_info, output_folder_path=output_folder_path)
     df = x.run()
     if isinstance(df, pd.DataFrame):
         df.to_excel(output_folder_path / f"{pdf_path.stem}.xlsx", index=False)
